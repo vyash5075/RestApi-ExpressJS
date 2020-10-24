e6670d8f97ca9461ea31507a41568de57d3b08a4
vyash5075 <vyash5075@gmail.com>
2020-10-24 09:44:34 +0530

    initial build

--- api/models/user.js
+++ api/models/user.js
@@ -4,7 +4,9 @@ const userSchema = mongoose.Schema({
     _id: mongoose.Schema.Types.ObjectId,
     email: {
         type:String,
-        required:true
+        required:true,
+        unique:true,
+        match:/[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$/
     },
     password: {
                 type:String,
--- api/routes/user.js
+++ api/routes/user.js
@@ -5,35 +5,74 @@ const User=require('../models/user');
 const  bcrypt= require('bcrypt');
 
 router.post('/signup',(req,res,next)=>{
-    bcrypt.hash(req.body.password,10,(err,hash)=>{
-        if(err){
-            return res.status(500).json({
-                error:err
+    User.find({email:req.body.email}).exec()
+    .then(user=>{
+        if(user.length>=1){
+            return res.status(409).json({
+                message:'user exists'
             })
-
         }
         else{
-            const user=new User({
-   
-                _id:new mongoose.Types.ObjectId(),
-                email:req.body.email,
-                password:hash
-            });
-            user.save()
-            .then(result=>{
-                console.log(result);
-                res.status(201).json({
-                    message:'User Created'
-                })
+            bcrypt.hash(req.body.password,10,(err,hash)=>{
+                if(err){
+                    return res.status(500).json({
+                        error:err
+                    })
+        
+                }
+                else{
+                    const user=new User({
+           
+                        _id:new mongoose.Types.ObjectId(),
+                        email:req.body.email,
+                        password:hash
+                    });
+                    user.save()
+                    .then(result=>{
+                        console.log(result);
+                        res.status(201).json({
+                            message:'User Created',
+                            _id:result._id,
+                            email:result.email
+
+                        })
+                    })
+                    .catch(err => {
+                        console.log(err);
+                        res.status(500).json({
+                          error: err
+                        });
+                      }); 
+                }
             })
-            .catch(err => {
-                console.log(err);
-                res.status(500).json({
-                  error: err
-                });
-              }); 
         }
     })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      });
+    
 
+})
+
+
+router.delete('/:userId',(req,res)=>{
+    User.remove({_id:req.params.userId})
+    .exec()
+    .then(user=>{
+        res.status(200).json({
+            message:'user deleted',
+           
+        })
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json({
+          error: err
+        });
+      }); 
+   
 })
 module.exports=router;
